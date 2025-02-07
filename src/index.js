fe363de2e410a98ae9d71de9e2d70e227a644e26
ali sheikh <alisheikh.astechware@gmail.com>
2025-02-07 11:19:10 +0500

    add bootstrap dependency and integrate styles in App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 const root = document.getElementById('root');
 createRoot(root).render(
